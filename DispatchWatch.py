e100f8066da64db71b554fd3becd8a77648dd802
DOCraven <brg.tman@gmail.com>
2020-03-19 10:38:35 +1100

    download fcn works

--- DispatchWatch.py
+++ DispatchWatch.py
@@ -77,44 +77,41 @@ def fileScanner(path):
         break
     return f
 
-def Downloader(CWD):
-    """Downloads the most recent predispatch zip from NEMWEB"""
-    #https://stackoverflow.com/questions/36161009/how-do-i-automate-downloading-a-zipped-file-from-a-website - stolen from this site
-    ### STEP 1 - set up the env
-    URL = 'http://nemweb.com.au/Reports/Current/PredispatchIS_Reports/'
-    root = 'http://nemweb.com.au/'
-    download_folder = CWD + '\\DL' #where the folder gets downloaded to 
+def downloader(): 
+    """ downloads last ZIPFILE in given URL"""
     
-    ### STEP 2 - find files 
-    full_url = root + zip_file #concat the full url
-    r = requests.get(URL) #open webpage
-    soup = BeautifulSoup(r.text, 'lxml') #scrape webpage
-    all_hrefs = soup.find_all('a') # find everything
-    all_links = [link.get('href') for link in all_hrefs] #magic happens 
-    zip_files = [dl for dl in all_links if dl and '.zip' in dl] #find all zips 
+    ## 
     
-    ### STEP 3 - DOWNLOAD FILES 
-    zip_file = zip_files[-1] #get last zip file in the from the list 
+    ### STEP 1 set up the env
+    cwd = os.getcwd()#get current dir
+    download_folder = cwd + "\\DL" 
+    root = 'http://nemweb.com.au/' #ENTER THE ROOT OF THE WHOLE DIR (ie, just to the TLD)
+    URL = "http://nemweb.com.au/Reports/Current/PredispatchIS_Reports/"
 
+    if not os.path.exists(download_folder): #make DL folder if it does not exist
+        os.makedirs(download_folder)
+    
+    ### STEP 2 - Scrape for URLS 
+    r = requests.get(URL) 
+    soup = BeautifulSoup(r.text, 'lxml')
+    
+    all_hrefs = soup.find_all('a')
+    all_links = [link.get('href') for link in all_hrefs]
+    zip_files = [dl for dl in all_links if dl and '.zip' in dl]
+
+    ### STEP 3 - Download last ZIP file 
+    zip_file = zip_files[-1] #get last zip file
+    full_url = root + zip_file #concat the full url
+    r = requests.get(full_url)
     zip_filename = os.path.basename(zip_file)
     dl_path = os.path.join(download_folder, zip_filename)
     with open(dl_path, 'wb') as z_file:
         z_file.write(r.content)
 
-    return #nothng 
 
-def Zipper(): 
-    """Unzips the file, into the DL directory, and deletes the original zip"""
-    cwd = os.getcwd()
-    DL = cwd + '\\DL'
-    extract = cwd + '\\OUT'
-    files = fileScanner(DL)
-    zips = files[-1]
-    ZIPpath = DL + '\\' + zips
-    with zipfile.ZipFile(ZIPpath, 'r') as zip_ref:
-        zip_ref.extractall(extract) 
-        
-    return #nothing 
+
+    return #nothin 
+
 
 
 
@@ -145,10 +142,10 @@ def DispatchWatch():
     DL = cwd + '\\DL'
     deleter(DL)
     ### STEP 2 - download data
-    Downloader(cwd) #download most recent upload from NEMWEB
+    downloader() #download most recent upload from NEMWEB
 
     ### STEP 3 - unzip data, load into memory as csv
-    Zipper()
+    # Zipper()
     
     ### STEP 4 - modify pd (will be the hard one)
     # Organiser()
